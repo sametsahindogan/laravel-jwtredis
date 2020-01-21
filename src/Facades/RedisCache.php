da7c1a6f6819754c715cb52310c5fc5907b78d97
sametsahindogan <sametsahindogan@gmail.com>
2020-01-21 17:40:33 +0300

    annotation

--- src/Facades/RedisCache.php
+++ src/Facades/RedisCache.php
@@ -5,8 +5,15 @@ namespace Sametsahindogan\JWTRedis\Facades;
 use Illuminate\Support\Facades\Facade;
 use Sametsahindogan\JWTRedis\Contracts\RedisCacheContract;
 
+/**
+ * Class RedisCache
+ * @package Sametsahindogan\JWTRedis\Facades
+ */
 class RedisCache extends Facade
 {
+    /**
+     * @return string
+     */
     public static function getFacadeAccessor()
     {
         return RedisCacheContract::class;
